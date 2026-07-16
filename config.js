c2eadee7461cc643bd00d0d05d12bafad02a7fc1
tbzenit55 <ggbbb2561@gmail.com>
2026-07-16 09:51:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const config = {
+  BOT_NAME: process.env.BOT_NAME || "ZENIT X BOT",
+  OWNER_NAME: process.env.OWNER_NAME || "ZENIT",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "",
+  PREFIX: process.env.PREFIX || ".",
+  MODE: process.env.MODE || "public",
+  PORT: process.env.PORT || 3000,
+  VERSION: "1.0.0"
+};
+
+export default config;
