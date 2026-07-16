3aee6f4d115d3f08ebfcf898ff6a713d231bf558
tbzenit55 <ggbbb2561@gmail.com>
2026-07-16 09:56:18 +0530

    Add startup function with logging and banner

--- core/startup.js
+++ core/startup.js
@@ -1,1 +1,12 @@
+import banner from "./banner.js";
+import logger from "./logger.js";
 
+export default function startup() {
+    banner();
+
+    logger.success("ZENIT X BOT Booting...");
+    logger.info("Loading Core System...");
+    logger.info("Loading Plugins...");
+    logger.info("Loading Configuration...");
+    logger.success("Core Loaded Successfully.");
+}
