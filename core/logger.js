918e88f04f5eb1400f98cceb4e97a605456af9a5
tbzenit55 <ggbbb2561@gmail.com>
2026-07-16 09:53:49 +0530

    Add logger utility with time-stamped methods

--- core/logger.js
+++ core/logger.js
@@ -1,1 +1,23 @@
+const time = () => {
+  return new Date().toLocaleTimeString();
+};
 
+export const logger = {
+  info(message) {
+    console.log(`[${time()}] [INFO] ${message}`);
+  },
+
+  success(message) {
+    console.log(`[${time()}] [SUCCESS] ${message}`);
+  },
+
+  warn(message) {
+    console.log(`[${time()}] [WARNING] ${message}`);
+  },
+
+  error(message) {
+    console.log(`[${time()}] [ERROR] ${message}`);
+  }
+};
+
+export default logger;
